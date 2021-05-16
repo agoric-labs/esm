0412aa3b8847549d9e6e2eec2582d7dc7fb5a548
Michael FIG <mfig@agoric.com>
2021-05-15 20:28:40 -0600

    chore: compile files to publish

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-/* eslint strict: off, node/no-unsupported-features: ["error", { version: 6 }] */
-"use strict"
-
-module.exports = require("./esm.js")
+"use strict";module.exports=require("./esm.js");
